e5301ef52b40acae4d7026c86fa3b40bb7f8f377
mealthebear <mealthebear@gmail.com>
2020-07-29 16:47:26 -0700

    Implemented latitude and longitude validators
    Co-authored-by: brianton7 <brianton7@gmail.com>

--- squareMaker.js
+++ squareMaker.js
@@ -1,4 +1,3 @@
-const haversine = require('haversine');
 const oneDegreeOfLat = 24901 / 360;
 
 const degreesToRadians = (lat) => {
@@ -22,12 +21,61 @@ const findNextSquare = numOfDots => {
   return Math.ceil(Math.sqrt(numOfDots));
 }
 
+const validateLongitude = lon => {
+  if (lon > 180) {
+    lon -= 360;
+  } else if (lon < -180) { 
+    lon += 360;
+  }
+  return lon;
+}
+
+const validateLatitude = (lat, lon, incrementer) => {
+  if (lat > 90) {
+    lat = 180 - lat;
+    incrementer *= -1;
+    if (lon > 0) {
+      lon = lon - 180;
+    } else {
+      lon = lon + 180;
+    }
+  } else if (lat < -90) {
+    lat = -180 - lat;
+    incrementer *= -1;
+    if (lon > 0) {
+      lon = lon - 180;
+    } else {
+      lon = lon + 180;
+    }
+  }
+
+  return {
+    currentLat: lat,
+    currentStartingLon: lon,
+    currentLatIncrementer: incrementer,
+  }
+}
+
 const squareMaker = (numOfDots, distance, lat, lon) => {
   let elevationPoints = [];
   const squareSize = findNextSquare(numOfDots);
   let startingLat = lat + distanceToLat(distance);
-  let startingLon = lon - distanceToLon(distance, lat);
+  let startingLon = validateLongitude(lon - distanceToLon(distance, lat));
   let trueStartingLon = startingLon;
   let latIncrementer = (distanceToLat(distance) * 2) / squareSize;
   let lonIncrementer = (distanceToLon(distance, lat) * 2) / squareSize;
+
+  for (let i = 0; i < squareSize; i++) {  
+    // check that startingLat is valid
+    let {currentLat, currentStartingLon, currentLatIncrementer} = validateLatitude(startingLat, trueStartingLon, latIncrementer);
+    startingLat = currentLat;
+    trueStartingLon = currentStartingLon;
+    latIncrementer = currentLatIncrementer; 
+
+    // set startingLon to trueStartingLon
+    startingLon = trueStartingLon;
+    for (let j = 0; j < squareSize; j++) {
+     
+    }
+  }
 }
